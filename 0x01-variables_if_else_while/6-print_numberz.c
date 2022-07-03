ee7fb6fcf8bd72aede0006ca19460f461e54cab9
Emmanuel <ochigboje@gmail.com>
2022-07-02 18:32:44 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
 	return (0);
 }
