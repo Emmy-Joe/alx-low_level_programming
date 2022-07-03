0370e99b69bc92e16e4398abca23e6f5423f165b
Emmanuel <ochigboje@gmail.com>
2022-07-02 21:27:44 -0700

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, c, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (c = '1'; c <= '9'; c++)
+		{
+			for (k = '2'; k <= '9'; k++)
+				if (i < c && c < k)
+				{
+					putchar(i);
+					putchar(c);
+					putchar(k);
+					
+					if (i != '7' && c != '8')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
