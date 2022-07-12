f2189666b88b178cea177f5a6754601556375075
Emmanuel <ochigboje@gmail.com>
2022-07-12 00:50:19 -0700

    Write a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,11 @@
 
 /**
  * main - deferencing pointers.
- * reset_to_98 - Updates the value it points to 98
+ *
+ * reset_to_98 -> Updates the value it points to 98.
+ *
  *  @n: pointer to n
+ *
  *  Return: void
  */
 
