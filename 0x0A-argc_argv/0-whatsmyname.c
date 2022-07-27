824682b66c9c2a644fba28d0ba6196801b591e11
Emmanuel <ochigboje@gmail.com>
2022-07-27 00:21:41 -0700

    hot fix: Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 /**
  * main - prints its name, followed by a new line.
  * @argc: argc parameter
- * @argv: array of command list
+ * @*argv: array of command list
  * Return: 0 success
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc ; i++)
 	{
 		printf("%s\n", *argv);
