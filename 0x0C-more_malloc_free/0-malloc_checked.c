c6c99429b7db896048ba2844c8e2a6c738a7d76c
Emmy-Joe <ochigboje@gmail.com>
2022-07-29 05:28:03 +0100

    Task-0 second commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked -> check for malloc
+ * @b: parameter b
+ * 
+ * Return: success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    void *p;
+    p = malloc(b);
+    if (p == NULL)
+        exit(98);
+    return (p);
+}
