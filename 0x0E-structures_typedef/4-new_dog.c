47b5bea4ea4c21fdb27248d87f48b4fa675ef84b
Emmanuel <ochigboje@gmail.com>
2022-08-02 22:07:54 -0700

    task-4: incomplete

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+/**
+ * new_dog - a function that creates a new dog
+ * get len of name + owner, malloc them, cpy name + owner to new
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
