49a98938d49f1a2edec4b5125316c462bf868394
Emmanuel <ochigboje@gmail.com>
2022-08-12 21:22:14 -0700

    Task-0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t cont = 0;
+	size_t nodes = 0;
 
-	while (h);
+	while (h)
 	{
-		if (h->str)
-			printf("[%u] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		cont++;
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
 		h = h->next;
 	}
-	return (cont);
+	return (nodes);
 }
