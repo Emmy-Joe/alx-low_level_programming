6e887f1ece25171cde6132c6dfab9b9be728e88b
Emmanuel <ochigboje@gmail.com>
2022-08-22 05:16:51 -0700

    Task-0 correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	file = open(filename, O_RDONLY);
-	if (filename == -1)
+	if (file == -1)
 	{
 		free(text);
 		return (0);
